e2dfae7b3e8c072166983c1b8d67f4b9616b6c64
Jason Jiang <jasonjiang316@gmail.com>
2022-04-16 22:17:22 -0400

    adding new customers to old tables

--- src/Table.java
+++ src/Table.java
@@ -10,31 +10,32 @@ public class Table {
     private static boolean available = true;
     private long time;
 
-    public Table(String i, long time) {
+    public Table(String i, long time) { // constructor
         setName("Table " + i);
         this.time = time;
     }
 
-    public void setName(String string) {
+    public void setName(String string) { // sets table id
         this.id = string;
     }
 
-    public String getName() {
+    public String getName() { // returns table id
         return id;
     }
 
-    public void addCustomer(Customer c) {
+    public void addCustomer(Customer c) { // adds customer to table
         if (seats > 0) {
             seats--;
             customerSeated.add(c);
             c.setSeated();
         }
-        if (seats == 0) {
+        if (seats == 0) { // if no more seats available set table as not available
             setNotAvailable();
         }
     }
 
-    public void assignEmployee(DineIn_Employee e) {
+    public void assignEmployee(DineIn_Employee e) { // asigns employeee to table and make it so no more employees can be
+                                                    // assigned to the table
         while (assigning) {
             assigning = false;
             if (assignedEmployee == null) {
@@ -46,31 +47,31 @@ public class Table {
         }
     }
 
-    public Object getAssignedEmployee() {
+    public Object getAssignedEmployee() { // return assigned employee
         return assignedEmployee;
     }
 
-    public int getSeats() {
+    public int getSeats() { // returns number of seats
         return seats;
     }
 
-    public void addSeats(int i) {
+    public void addSeats(int i) { // adds seats to table
         seats += i;
     }
 
-    public void setSeats() {
+    public void setSeats() { // sets number of seats
         this.seats = seats - 1;
     }
 
-    public void setNotAvailable() {
+    public void setNotAvailable() { // sets table as not available
         available = false;
     }
 
-    public void setAvailable() {
+    public void setAvailable() { // sets table as available
         available = true;
     }
 
-    public boolean getAvailable() {
+    public boolean getAvailable() { // returns if table is available
         return available;
     }
 
